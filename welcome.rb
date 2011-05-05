d5b48c68be043a02dbc32e428f2a548a15202b2b
Ana <ana@ana-XPS-M1530.(none)>
2011-05-04 22:37:39 -0500

    Set validations

--- welcome.rb
+++ welcome.rb
@@ -3,18 +3,26 @@ require 'sinatra/activerecord'
 require 'sinatra/base'
 require 'haml'
 
+
+
 class Contact < ActiveRecord::Base
-end
+	validates_presence_of :name, :email
+	validates_uniqueness_of :email, :twitter
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ , :message => "Correo no valido"
 
-  get '/' do   
-		haml :index, :layout => false
-  end
+	get '/' do   
+		haml :index
+	end
 
-  post '/contact' do
-		@info = Contact.new(:name => params[:name], :email => params[:email], :twitter => params[:twitter])
+	post '/contact' do
+		@info = Contact.new(:name => params[:name], :email => params[:email], :twitter => params[:twitter]) 
 		if @info.save
 				haml :thank_you
 			else
 				haml :index
 		end
-  end
+	end
+
+end
+
+
