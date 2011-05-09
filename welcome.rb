2b7c314b427856bd876b7cc27fdb4f7d94bd4f00
Ana <ana@ana-XPS-M1530.(none)>
2011-05-09 09:44:48 -0500

    try 402929292

--- welcome.rb
+++ welcome.rb
@@ -4,8 +4,6 @@ require 'sinatra/base'
 require 'active_record'
 require 'haml'
 
-dbconfig = YAML.load(File.read('config/database.yml'))
-ActiveRecord::Base.establish_connection dbconfig['production']
 
 class Contact < ActiveRecord::Base
 	validates_presence_of :name, :email
