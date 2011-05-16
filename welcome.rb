756b8219420ece526be463edd6eea26ac5c9af28
Ana Rosas <ana@ana-XPS-M1530.(none)>
2011-05-15 23:43:01 -0500

    fix for twitter uniqueness

--- welcome.rb
+++ welcome.rb
@@ -13,7 +13,7 @@ ActiveRecord::Base.establish_connection dbconfig['production']
 
 class Contact < ActiveRecord::Base
 	validates_presence_of :nombre, :email, :message => "no puede estar vacio/ "
-	validates_uniqueness_of :email, :twitter, :message => "ya registrado/ "
+	validates_uniqueness_of :email, :message => "ya registrado/ "
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ , :message => "no valido/ "
 end
 	get '/' do   
